646010b015567d27b216a11b8ec66aeb705baea1
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-15 01:27:25 +0100

    up

--- loginsystemtest/user_management/urls.py
+++ loginsystemtest/user_management/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
 
+    path('about/', CustomLoginView.as_view( template_name='users/about.html'), name='about'),
+
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
