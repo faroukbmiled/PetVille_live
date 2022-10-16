8b981c4bdd94bc23d0a5f9e015c75a22fe58201d
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-10-16 13:10:30 -0700

    Delete loginsystemtest directory

--- loginsystemtest/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-
-
-class CurrentUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name')
