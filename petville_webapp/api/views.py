50b21485dd4471e7f6f849cf33972087c795f20c
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-17 17:19:21 +0100

    up

--- petville_webapp/api/views.py
+++ petville_webapp/api/views.py
@@ -29,18 +29,17 @@ class LastLoginSerializer(viewsets.ReadOnlyModelViewSet):
 class ServerStatus(viewsets.ViewSet):
 
     def list(self, request, format=None):
-        
-        content_ok = {"status": "OK", "code": "200"}
         #return Response(content, status=status.HTTP_200_OK)
         try:
             r = requests.get('http://127.0.0.1:8000/')
+            content_err = {"status": "error", "code": "{}".format(r.status_code)}
             r.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            content_err = {"status": "error", "code": {r.status_code}}
             pass
             return Response(content_err, status=status.HTTP_404_NOT_FOUND)
-        
-        return Response(content_ok, status=status.HTTP_200_OK)
+        else:
+            content_ok = {"status": "OK", "code": "{}".format(r.status_code)}
+            return Response(content_ok, status=status.HTTP_200_OK)
 
 
 
