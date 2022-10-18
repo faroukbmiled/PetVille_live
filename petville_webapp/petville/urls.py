f078bdda6fcb2bd0c2d42491c63ff7512119e223
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-18 14:06:07 +0100

    everything working

--- petville_webapp/petville/urls.py
+++ petville_webapp/petville/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('', home, name='petville-home'),
     path('register/', RegisterView.as_view(), name='petville-register'),
     path('profile/', profile, name='petville-profile'),
+    path('find/', CustomLoginView.as_view( template_name='petville/find.html'), name='find'),
     path('about/', CustomLoginView.as_view( template_name='petville/about.html'), name='about'),
+    path('vet/', CustomLoginView.as_view( template_name='petville/vet.html'), name='vet'),
+    path('gallery/', CustomLoginView.as_view( template_name='petville/gallery.html'), name='gallery'),
 ]
