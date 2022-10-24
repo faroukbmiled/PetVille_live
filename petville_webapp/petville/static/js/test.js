f31d710656cee5e00c0123b58d19a9c462b5776d
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-24 22:11:10 +0100

    fix

--- petville_webapp/petville/static/js/test.js
+++ petville_webapp/petville/static/js/test.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$(function() {setTimeout(function() {$("#menu").hide('blind', {}, 5)}, 5);}); 
+$(function() {setTimeout(function() {$("#menu").hide('blind', {}, 30)}, 30);}); 
 $(function() {setTimeout(function() {$("#h_div").hide('blind', {}, 35)}, 35);}); 
 $('#menu').hover(function(){$('#h_div').fadeIn("slow");}, function(){$('#h_div').fadeOut("slow");});
 $('#dropd').on('change', function(){
