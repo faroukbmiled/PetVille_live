e175cb784152b9ee82d2e50ca8e092fef716e4bd
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-24 21:33:48 +0100

    fuck

--- petville_webapp/petville/forms.py
+++ petville_webapp/petville/forms.py
@@ -55,7 +55,7 @@ class RegisterForm(UserCreationForm):
     city = forms.CharField(max_length=100,
                                required=True,
                                widget=forms.TextInput(attrs={'placeholder': 'Your Adress','class': 'form-control', 'id': 'adrr'}))
-    state = forms.ChoiceField(choices=blank_choice+GOVERNORATE_CHOICES, required=True, widget=forms.Select(attrs={'class':'form-dropdown form-control', 'id': 'dropd'}))
+    state = forms.ChoiceField(choices=blank_choice+GOVERNORATE_CHOICES, required=True, widget=forms.Select(attrs={'class':'form-dropdown form-control'}))
     
     location = PlainLocationField(attrs={'style': 'position: absolute;left: -999em;'}, based_fields=['city', 'state'],
                                   initial='36.80105674280464, 10.181972264198441')
